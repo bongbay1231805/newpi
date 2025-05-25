b00805427bb78e227cb636d2f626471df266b1b6
Lionel <phamngoctuong1805@gmail.com>
2025-05-25 18:23:04 +0700

    Update Source 3

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
-import createMiddleware from 'next-intl/middleware';
-import {locales, defaultLocale} from './next-intl.config';
-export default createMiddleware({
-  locales,
-  defaultLocale
-});
-export const config = {
-  matcher: ['/', '/(en|vi)/:path*']
-};
+// import createMiddleware from 'next-intl/middleware';
+// import intlConfig from './next-intl.config'; // Rename the import to avoid conflict
+// export default createMiddleware({
+//   locales: intlConfig.locales, // Access locales from the imported intlConfig object
+//   defaultLocale: intlConfig.defaultLocale // Access defaultLocale from the imported intlConfig object
+// });
+// export const config = {
+//   matcher: ['/', '/(en|vi)/:path*']
+// };
