cc7a3effe415446f9925a33602e86f6840e2baae
Lionel <phamngoctuong1805@gmail.com>
2025-05-25 16:15:31 +0700

    Edit error 4

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+const withNextIntl = require('next-intl/plugin')('./next-intl.config.js');
+module.exports = withNextIntl({
   experimental: {
-    serverActions: true
+    serverActions: {}
   }
-};
-module.exports = nextConfig;
+});
