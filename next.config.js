b280fd31234f5a2762944fca806dd056c8288c98
Lionel <phamngoctuong1805@gmail.com>
2025-05-25 16:05:20 +0700

    Edit error 1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-const {withNextIntl} = require('next-intl/plugin');
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: true
   }
 };
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
